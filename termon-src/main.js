bc3409d748441a86ab20c536b3a337350dcd4646
PrabhuUdurg <egorpeshiy@gmail.com>
2024-10-14 00:45:11 +0200

    Functionality Done

--- termon-src/main.js
+++ termon-src/main.js
@@ -1,6 +1,12 @@
+#!/usr/bin/env node
+
 import fs from 'fs';
 import { spawn } from 'child_process';
 import inquirer from 'inquirer';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { search, Separator } from '@inquirer/prompts';
+import chalk from 'chalk';
 
 
 function createNewFile() {
@@ -39,6 +45,38 @@ function createNewFile() {
         .catch(error => {
             console.error(error);
         });
+};
+
+
+async function openFile() {
+    const __filenameNew = fileURLToPath(import.meta.url)
+    const __dirnameNew = path.dirname(__filenameNew)
+    const files = fs.readdirSync(__dirnameNew);
+
+    const answer = await search({
+        message: 'Choose a file',
+        source: (term) => files,
+    });
+    const vim = spawn('vim', [answer], { stdio: 'inherit' });
+
 }
 
-createNewFile();
+inquirer
+    .prompt([
+        {
+            type: 'list',
+            name: "menu",
+            message: chalk.magenta("What are we doing today? :3"),
+            choices: ['Create New File', 'Open File'],
+        }
+    ])
+    .then(answers => {
+        if (answers.menu === "Create New File") {
+            createNewFile();
+        } else if (answers.menu === "Open File") {
+            openFile();
+        }
+    })
+    .catch(error => {
+        console.error(error);
+    });
